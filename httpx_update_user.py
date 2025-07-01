f2df35189d40348fa522425ca564a0f2c94075b5
Alexander Kadenev <crohntv@gmail.com>
2025-07-01 16:06:51 +0700

    6.2 Практикуемся в работе с HTTPX Практикуемся в работе с HTTPX: обновление пользователя

---
+++ httpx_update_user.py
@@ -0,0 +1,50 @@
+import httpx
+from tools.faker import get_random_email
+
+create_user_payload = {
+    "email": get_random_email(),
+    "password": "string",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string"
+}
+
+create_user_response = httpx.post("http://localhost:8000/api/v1/users",
+                                  json=create_user_payload)
+
+create_user_response_data = create_user_response.json()
+
+print(f"Create user data: {create_user_response_data}")
+print(f"Status code: {create_user_response.status_code}")
+
+login_payload = {
+    "email": create_user_response_data["user"]["email"],
+    "password": "string"
+}
+
+login_response = httpx.post(
+    "http://localhost:8000/api/v1/authentication/login",
+    json=login_payload)
+login_response_data = login_response.json()
+
+print(f"Login data: {login_response_data}")
+print(f"Status code: {login_response.status_code}")
+
+update_user_headers = {
+    "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
+}
+update_user_payload = {
+    "email": get_random_email(),
+    "lastName": "Test",
+    "firstName": "Test",
+    "middleName": "Test"
+}
+
+update_user_response = httpx.patch(
+    f"http://localhost:8000/api/v1/users/{create_user_response_data["user"]["id"]}",
+    headers=update_user_headers,
+    json=update_user_payload)
+update_user_response_data = update_user_response.json()
+
+print(f"Update user data: {update_user_response_data}")
+print(f"Status code: {update_user_response.status_code}")
