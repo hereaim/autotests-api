13022948dac4ea9948ca05efa689551fec06d2c7
Alexander Kadenev <crohntv@gmail.com>
2025-07-11 12:25:52 +0700

    Фикс files_client.py

--- clients/files/files_client.py
+++ clients/files/files_client.py
@@ -28,7 +28,7 @@ class FilesClient(ApiClient):
         return self.post('/api/v1/files',
                          data=request.model_dump(by_alias=True,
                                                  exclude={'upload_file'}),
-                         files={"upload_file": open(request['upload_file'],
+                         files={"upload_file": open(request.upload_file,
                                                     'rb')})
 
     def delete_file_api(self, file_id: str) -> Response:
