6d6e0d79314f57f91de073ad1ad69f01429619ed
Alexander Kadenev <crohntv@gmail.com>
2025-08-12 15:44:13 +0700

    exercises_fixture

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 pytest_plugins = ("fixtures.authentication",
                   "fixtures.users",
                   "fixtures.files",
-                  "fixtures.courses")
+                  "fixtures.courses",
+                  "fixtures.exercises")
